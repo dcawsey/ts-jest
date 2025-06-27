098fe6a515cc393a62fee9c3514a6498047da6b8
Dylan Cawsey <dylancawsey@gmail.com>
2025-06-27 20:58:36 +1000

    Remove build directory from version control

--- dist/index.js
+++
@@ -1,3 +0,0 @@
-import { add } from './utils.js';
-console.log('add(2, 3)', add(2, 3));
-//# sourceMappingURL=index.js.map
--- dist/utils.js
+++
@@ -1,4 +0,0 @@
-export function add(firstArg, secondArg) {
-    return firstArg + secondArg;
-}
-//# sourceMappingURL=utils.js.map
